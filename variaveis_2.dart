7ab8b3a9a34ad4a7bbb5a5c2bd42bcbbec3c68a5
Rafael Rodegheri Colaço <114178864+colacoxw@users.noreply.github.com>
2023-06-06 22:22:58 -0300

    Create variaveis_2.dart
    O código 2/2 de variáveis, feito para finalizar o conceito de variáveis em Dart

---
+++ variaveis_2.dart
@@ -0,0 +1,14 @@
+main(){
+  var a = 2;
+  var b = 4.56;
+  var texto = "O valor da soma é: ";
+
+  print(texto + (a + b).toString());
+
+  print(a.runtimeType);
+  print(b.runtimeType);
+  print(texto.runtimeType);
+
+  print(a is int);
+  print(a is String);
+}
